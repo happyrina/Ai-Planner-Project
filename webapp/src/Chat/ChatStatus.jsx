a0e2d1737222b43c1a8fe076fdef69a234c48f4d
CEJSH <eun2763@naver.com>
2023-10-10 15:25:32 +0000

    mod

--- webapp/src/Chat/ChatStatus.jsx
+++ webapp/src/Chat/ChatStatus.jsx
@@ -1,6 +1,5 @@
 // Copyright (c) Microsoft. All rights reserved.
 
-// import { makeStyles } from "@fluentui/react-components";
 import { Animation } from "@fluentui/react-northstar";
 import React from "react";
 import styles from "./css/ChatStatus.module.css";
@@ -10,7 +9,6 @@ export const ChatStatus = () => {
   return (
     <Animation name="slideInCubic" keyframeParams={{ distance: "2.4rem" }}>
       <div className={styles.ChatStatusRoot}>
-        {/* <label>{message}</label> */}
         <TypingIndicator />
       </div>
     </Animation>
